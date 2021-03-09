fcdf21152ffc213ac3f0833edcd3d277ab9a15d6
Luke Harding <lukeharding990@gmail.com>
2021-03-09 01:23:03 +0000

    Hello

--- routes/index.js
+++ routes/index.js
@@ -2,5 +2,8 @@ const routes = require('express').Router();
 const apiv1 = require('./v1');
 
 routes.use('/v1', apiv1);
+routes.get('/', (req, res) => {
+	res.send('hello')
+});
 
 module.exports = routes;
