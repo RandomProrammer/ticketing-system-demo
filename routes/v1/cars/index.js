47427aa484dce4e6f4d56b505e726bacf8acdecb
luke110206 <lukeharding90@gmail.com>
2021-03-08 19:54:13 -0500

    Fixed not loading bug an updated data

--- routes/v1/cars/index.js
+++ routes/v1/cars/index.js
@@ -1,7 +1,7 @@
 const cars = require('express').Router();
 const all = require('./all');
 const single = require('./single');
-const findObject = require('../../../../utils/findObject');
+const findObject = require('../../../utils/findObject');
 
 cars.param('carId', findObject('car'));
 
